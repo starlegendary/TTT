4b1e688c8f636e07602596d06b2263a7339e52c9
starlegendary <billshum08@gmail.com>
2020-05-11 16:34:50 +0000

    time

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import numpy as np
 size = (3,3)
 board = np.zeros(size)
-history = {}
-
+history = [[]]*(size[0]*size[1])
+time = 0
+print(history)
 class player:
     def __init__(self,name = None, agent = None):
         self.name  = name
@@ -12,13 +14,16 @@ class prop:
         self.space = space
 
 def place(player,board,space):
+    global time
     board[space[0]][space[1]] = player.name
-def action(move,player,board,space,time):
-    move(player,board,space)
-    history[time] = prop(player.name, space)
+    time = time + 1
+    history[time].append(prop(player.name, space))
 
+def future(board):
+    space = np.where(board == 0)
+    return list(zip(space[0],space[1]))
 a = player( 1,None)
 b = player(-1,None)
-
-def future(board,time):
-    
+place(a,board,(1,1))
+place(b,board,(0,1))
+print(history)
