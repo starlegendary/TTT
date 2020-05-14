54ffae1d6f337581273843217b29daf643f47966
starlegendary <billshum08@gmail.com>
2020-05-14 09:10:14 +0000

    tree3

--- tree.py
+++ tree.py
@@ -3,29 +3,25 @@ import random
 size = 3
 board   = np.zeros(size**2)
 allboard  = {}
-tree   = {}
+allnode   = {}
+
 class node:
     def __init__(self,before,now):
         self.before = before
         self.now    = now
         self.value  = None
-    def future(self):
-        return list(map(lambda x: self.now + str(x),empty(self.now)))
+    def future(self): return np.where(allboard[self.now] == 0)[0]
 
-def empty(name):
-    return list(map(lambda x: str(x),np.where(allboard[name] == 0)[0]))
 def mark(time): return 1 if time % 2 == 0  else -1
+def rand(name): return random.choice(allnode[name].future())
 
-def nextrand(name):
-    nextname = random.choice(tree[name].future())
-    tree[nextname]  = node(name,nextname)
-    allboard[nextname] = allboard[name].copy()
+def nextmove(name,agent):
+    nextname = name + str(agent(name))
+    allnode[nextname]  = node(name,nextname)
+    allboard[nextname] = allboard[name]
     allboard[nextname][int(nextname[-1])] = mark(len(nextname))
     return nextname
-def win(num):
-    if   num ==  3 : return 1
-    elif num == -3 : return -1
-    return None
+
 def winner(board):
     result = []
     sth = np.reshape(board,(size,size))
@@ -38,13 +34,19 @@ def winner(board):
     result.append(0)
     for i in range(size):
         result[-1] += sth[i,size-1-i]
-    return win(max(result, key=abs)) #maximum absolute value
+    result = max(result, key=abs)
+    if   result ==  size : return 1
+    elif result == -size : return -1
+    return None
 allboard[''] = board
-tree[''] = node(None,'')
-print(board)
-a = nextrand('')
-print(board)
-b = nextrand(a)
-print(winner(allboard[b]))
-c = nextrand(b)
-print(winner(allboard[c]))
+allnode[''] = node(None,'')
+#a = nextrand(b)
+def allrand(name,agent):
+    while len(name) != size**2:
+        name = nextmove(name,agent)
+        print(name)
+        print(allboard[name])
+        winning = winner(allboard[name])
+        if winning != None: return winning
+    return 'Tie'
+print(allrand('',rand))
